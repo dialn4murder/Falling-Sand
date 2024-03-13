e1d9770addd6edb9f55232dad3665dd6b1860139
Owner <dylanchampion596@hotmail.com>
2024-03-13 18:10:17 +0000

    First Git iteration

--- Grid_Class.py
+++ Grid_Class.py
@@ -4,15 +4,20 @@ import square
 
 class Grid_Class:
     def __init__(self, width, height):
+        # Defines the width and height of the application
         self.width = width
         self.height = height
+        # Defines an empty and white square based on the square class
         self.empty_square = square.square(0, (0, 0, 0))
         self.white_square = square.square(1, (255, 255, 255))
+        # Calculates the maximum width / height of the grid based on application size and area of the squares
         self.column = self.width // self.empty_square.area
         self.row = self.height // self.empty_square.area
+        # Creates the grid with empty squares
         self.grid = [[self.empty_square for _ in range(self.column)] for _ in range(self.row)]
 
     def print_grid(self):
+        # Prints the grid based on square id
         for row in range(self.row):
             for column in range(self.column):
                 print(self.grid[row][column].square_id, end=" ")
@@ -21,23 +26,34 @@ class Grid_Class:
     def draw(self, screen):
         for row in range(self.row):
             for column in range(self.column):
+                # Gets current cell
                 cell_val = self.grid[row][column]
+                # Defines the cell area
                 cell_rect = pygame.Rect(column * self.white_square.area + 1, row * self.white_square.area + 1,
                                         self.white_square.area - 1, self.white_square.area - 1)
+                # Draws the square based on the squares colour value
                 pygame.draw.rect(screen, cell_val.colour, cell_rect)
+                # If its a white square
                 if cell_val.square_id == 1:
+                    # Makes sure the squares won't go out of bounds
                     if row < self.row-1:
+                        # Calls check fall makes sure it has an empty cell below the cell
                         self.check_fall(row, column)
 
     def draw_cursor(self, x, y):
+        # Calculates the grid position based on the x and y value
         row = x // self.empty_square.area
         column = y // self.empty_square.area
+        # Makes the cell a white square where the cursor was
         self.grid[column][row] = self.white_square
 
     def check_fall(self, row, column):
+        # Makes sure the cell below is empty
         if self.grid[row + 1][column] == self.empty_square:
+            # Adds to the counter which slows the animation
             self.grid[row][column].counter += 1
-            print(self.grid[row][column].counter)
+            # Checks if the counter is dividable
             if self.grid[row][column].is_dividable():
+                # Swaps cells
                 self.grid[row][column] = self.empty_square
                 self.grid[row + 1][column] = self.white_square
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import Grid_Class
 
+# Typical pygame boilerplate
 pygame.init()
 screen_width = 720
 screen_height = 1000
@@ -8,24 +9,26 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 running = True
 
+# Calls the grid class which creates the grid and draws onto the grid
 game_grid = Grid_Class.Grid_Class(screen_width, screen_height)
 
 while running:
-    # poll for events
+
     # pygame.QUIT event means the user clicked X to close your window
-    keys = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    # When left mouse is pressed it will get the position of the mouse
         if pygame.mouse.get_pressed()[0]:
             pos = pygame.mouse.get_pos()
+            # It will call the method to draw a square where the mouse was
             game_grid.draw_cursor(pos[0], pos[1])
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("gray")
 
-    # RENDER YOUR GAME HERE
+    # Draws the grid, empty squares and white squares
     game_grid.draw(screen)
 
     # flip() the display to put your work on screen
